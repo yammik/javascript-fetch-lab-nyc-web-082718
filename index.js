e29b5719fe2ed55fad761de5bf195ef9e9aeccc0
yammik <mkim0514@gmail.com>
2018-10-15 01:26:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
 function getIssues() {
+  return fetch('https://api.github.com/repos/yammik/javascript-fetch-lab/issues', {
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json()).then(json => showIssues(json));
 }
 
 function showIssues(json) {
+  const issues = `<ul>${json.map(issue => `<li>${issue.title} - ${issue.body}</li>`).join("")}</ul>`;
+  document.getElementById("issues").innerHTML = issues;
 }
 
 function createIssue() {
+  let issueTitle = document.getElementById('title').value;
+  let issueBody = document.getElementById('body').value;
+
+  return fetch('https://api.github.com/repos/yammik/javascript-fetch-lab/issues', {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`
+    },
+    body: JSON.stringify({title: issueTitle, body: issueBody})
+  }).then(e => getIssues());
 }
 
 function showResults(json) {
+  const result = JSON.stringify(json)
+  document.getElementById("results").innerHTML = result;
+  return json;
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
-  //use fetch to fork it!
+  return fetch('https://api.github.com/repos/learn-co-curriculum/javascript-fetch-lab/forks', {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${getToken()}`
+    }
+  }).then(res => res.json()).then(json => showResults(json)).then(json => showForkedRepo(json));
 }
 
 function getToken() {
-  //change to your token to run in browser, but set
-  //back to '' before committing so all tests pass
-  return ''
+  return '';
+}
+
+function showForkedRepo(json) {
+  let resultsBody = document.getElementById('results').innerHTML;
+  document.getElementById('results').innerHTML = `<a href="${json.html_url}">Link to forked repo</a><br>` + resultsBody;
 }
--- test/index-test.js
+++ test/index-test.js
@@ -50,7 +50,7 @@ describe('index', () => {
       const url = fetchSpy.calls[0].arguments[0]
       expect(url).toMatch(/api.github.com\/repos\/learn-co-curriculum\/javascript-fetch-lab/)
       const opts = fetchSpy.calls[0].arguments[1]
-      expect(opts.method).toMatch(/post/)
+      expect(opts.method).toMatch(/POST/)
       expect(opts.headers).toMatch(/Authorization: token\s./)
     })
 
@@ -63,7 +63,7 @@ describe('index', () => {
       expect(url).toMatch(/javascript-fetch-lab\/issues/)
       expect(url).toNotMatch(/learn-co-curriculum/)
       const opts = fetchSpy.calls[0].arguments[1]
-      expect(opts.method).toMatch(/post/)
+      expect(opts.method).toMatch(/POST/)
       expect(opts.headers).toMatch(/Authorization: token\s./)
       expect(opts.body).toMatch(/test body/)
     })
